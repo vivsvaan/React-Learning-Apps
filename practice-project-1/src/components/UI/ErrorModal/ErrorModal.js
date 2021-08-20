b803958af85360a7e5268ca5b05955ef7652cc24
vivsvaan <sharmavivsvaan@gmail.com>
2021-08-21 01:00:44 +0530

    updated practice project

--- practice-project-1/src/components/UI/ErrorModal/ErrorModal.js
+++ practice-project-1/src/components/UI/ErrorModal/ErrorModal.js
@@ -1,29 +1,47 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 
 import classes from "./ErrorModal.module.css";
 
-function ErrorModal(props) {
-    function dismissModal() {
-        props.onModalDismiss();
-    }
+function Backdrop(props) {
+    return <div className={classes.backdrop} onClick={props.dismissModal} />;
+}
+
+function ModalOverlay(props) {
     return (
-        <div>
-            <div className={classes.backdrop} onClick={dismissModal} />
-            <Card className={classes.modal}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.actions}>
-                    <Button onClick={dismissModal}>Okay</Button>
-                </footer>
-            </Card>
-        </div>
+        <Card className={classes.modal}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.actions}>
+                <Button onClick={props.onConfirm}>Okay</Button>
+            </footer>
+        </Card>
+    );
+}
+
+function ErrorModal(props) {
+    return (
+        <React.Fragment>
+            {ReactDOM.createPortal(
+                <Backdrop dismissModal={props.onModalDismiss} />,
+                document.getElementById("backdrop-root")
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay
+                    title={props.title}
+                    message={props.message}
+                    onConfirm={props.onModalDismiss}
+                />,
+                document.getElementById("overlay-root")
+            )}
+        </React.Fragment>
     );
 }
 
--- practice-project-1/src/components/Users/AddUser/AddUser.js
+++ practice-project-1/src/components/Users/AddUser/AddUser.js
@@ -1,16 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../../UI/Button/Button";
 import Card from "../../UI/Card/Card";
 import ErrorModal from "../../UI/ErrorModal/ErrorModal";
+import Wrapper from "../../Helpers/Wrapper/Wrapper";
+
 import classes from "./AddUser.module.css";
 
 function AddUser(props) {
-    const [enteredUsername, setEnteredUsername] = useState("");
-    const [enteredAge, setEnteredAge] = useState("");
+    // const [enteredUsername, setEnteredUsername] = useState("");
+    // const [enteredAge, setEnteredAge] = useState("");
     const [error, setError] = useState(null);
+
+    const usernameInputRef = useRef();
+    const ageInputRef = useRef();
 
     function addUserHandler(event) {
         event.preventDefault();
+        const enteredUsername = usernameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
         if (
             enteredUsername.trim().length === 0 ||
             enteredAge.trim().length === 0 ||
@@ -23,22 +30,24 @@ function AddUser(props) {
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUsername("");
-        setEnteredAge("");
+        usernameInputRef.current.value = "";
+        ageInputRef.current.value = "";
+        // setEnteredUsername("");
+        // setEnteredAge("");
     }
 
-    function usernameChangeHandler(event) {
-        setEnteredUsername(event.target.value);
-    }
-    function ageChangeHandler(event) {
-        setEnteredAge(event.target.value);
-    }
+    // function usernameChangeHandler(event) {
+    //     setEnteredUsername(event.target.value);
+    // }
+    // function ageChangeHandler(event) {
+    //     setEnteredAge(event.target.value);
+    // }
     function modalDismissHandler() {
         setError(null);
     }
 
     return (
-        <div>
+        <Wrapper>
             {error && (
                 <ErrorModal
                     title={error.title}
@@ -53,15 +62,22 @@ function AddUser(props) {
                     <input
                         id="username"
                         type="text"
-                        value={enteredUsername}
-                        onChange={usernameChangeHandler}
+                        // value={enteredUsername}
+                        // onChange={usernameChangeHandler}
+                        ref={usernameInputRef}
                     />
                     <label htmlFor="age">Age</label>
-                    <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+                    <input
+                        id="age"
+                        type="number"
+                        // value={enteredAge}
+                        // onChange={ageChangeHandler}
+                        ref={ageInputRef}
+                    />
                     <Button type="submit">Add User</Button>
                 </form>
             </Card>
-        </div>
+        </Wrapper>
     );
 }
 
